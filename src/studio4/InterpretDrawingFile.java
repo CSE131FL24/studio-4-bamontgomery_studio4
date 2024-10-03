1970258d4bbc2034300a992de0b14b3e18cf185c
chasemontgomery <chasemontgomery@localhost>
2024-10-03 11:45:39 -0500

    Flag

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,23 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		
+		int R = in.nextInt();
+		int G = in.nextInt();
+		int B = in.nextInt();
+		
+		boolean filled = in.nextBoolean();
+		
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double w = in.nextDouble();
+		double h = in.nextDouble();
+		
+		StdDraw.setPenColor(R,G,B);
+		
+		if(filled) {
+			StdDraw.filledRectangle(x, y, w, h);
+		}
 	}
 }
